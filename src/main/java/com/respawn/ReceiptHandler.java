b71c47a8451dc865774cdec7f84ce0065bf7ee20
Frank Quigley <quiglefran@ecs.vuw.ac.nz>
2026-05-30 14:54:29 +1200

    Initialised Maven File Structure

--- src/main/java/com/respawn/ReceiptHandler.java
+++ src/main/java/com/respawn/ReceiptHandler.java
@@ -5,7 +5,6 @@ import java.io.ByteArrayOutputStream;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
-
 import javax.print.Doc;
 import javax.print.DocFlavor;
 import javax.print.PrintService;
